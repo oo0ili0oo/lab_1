17c56f94eb96e241b816e629e2736ee6c9655671
oo0ili0oo <80237064+oo0ili0oo@users.noreply.github.com>
2021-03-08 17:32:05 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,36 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
+double check(const string& x){
+    double y;
+    do
+    {
+        cin.clear();
+        cin.sync();
+        cout << "Enter " << x << ":";
+        cin>>y;
+        if(cin.fail())
+            cout<<"That is not number!\n";
+        else if ( y <= 0 )
+            cout << "That is not positive number!\n";
+    }while(cin.fail()||y<=0);
+    return y;
+}
+
 int main() {
     double height, distance, corner, speed;
     const double pi(3.1415), gravity(9.8);
 
-    std::cout << "Enter H" << std::endl;
-    std::cin >> height;
-
-    std::cout << "Enter L" << std::endl;
-    std::cin >> distance;
+    height = check("H");
+    distance = check("L");
 
     corner = (atan(4 * height / distance) * 180 / pi);
-    std::cout << corner << std::endl;
+    cout << "Corner is " << corner;
 
     speed = sqrt(gravity * distance / (sin(2 * corner * pi / 180)));
-    std::cout << speed << std::endl;
+    cout << "\n" << "Speed is " << speed;
 
     return 0;
 }
